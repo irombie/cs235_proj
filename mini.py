d559ec3e8e55e5db2653e0acb3237752e33d1df5
Irem Ergun <iremergun@Irems-MacBook-Pro.local>
2018-12-11 12:29:55 -0800

    Makefile

--- mini.py
+++ mini.py
@@ -11,6 +11,11 @@ from keras.utils import plot_model
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import itertools
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Run CNN on given dataset')
+parser.add_argument('--dataset_folder_path', metavar='N', type=str, help='Path to a folder which contains data.npy and labels.npy')
 
 genres =	{
   "metal": 0,
@@ -39,10 +44,6 @@ def construct_input_matrix(input_loc, input_h, input_w, channels, no_of_imgs):
                     img = cv2.imread(os.path.join(root1,f),cv2.IMREAD_COLOR) 
                     a[index,:,:,:] = img
                     index += 1
-                    '''
-                    if index % 10 == 0 and index != 0:
-                        break
-                    '''
     a = stats.zscore(a)
     a = np.nan_to_num(a)
     np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/data', a)
@@ -98,61 +99,32 @@ def construct_input_matrix_grayscale(input_loc, input_h, input_w, no_of_imgs):
                     img = cv2.imread(os.path.join(root1,f),cv2.IMREAD_GRAYSCALE) 
                     a[index,:,:] = img
                     index += 1
-                    '''
-                    if index % 10 == 0 and index != 0:
-                        break
-                    '''
     np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/data', a)
     np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/labels', labels)
     return a, labels
 
-'''
-data,labels = construct_input_matrix('/Users/iremergun/Desktop/ucr_classes/cs235/proj/genres_tripled',324,300,3,3000)
-print(data.shape)
-print(data)
-print(labels)
+def main(folder_path):
+    data_path = os.path.join(folder_path, "data.npy")
+    label_path = os.path.join(folder_path, "labels.npy")
+    data = np.load(data_path)
+    labels = np.load(label_path)
+    train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.10)
+    model = LeNet.build_model_lenet(324,300,3,10)
+    opt = Adam(lr=0.001)
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"]) #categorical_crossentropy
+    print("[INFO] training...")
+    model.fit(train_data, train_label, batch_size=64, epochs=10, verbose=1, validation_data=(test_data, test_label) )
+    (loss, accuracy) = model.evaluate(test_data, test_label, batch_size=64, verbose=1)
+    results = model.predict_classes(test_data, batch_size=64, verbose =1)
+    print("[INFO] accuracy: {:.2f}%".format(accuracy * 100))
+    print("loss: {}".format(loss))
+    '''Uncomment if you want to plot confusion matrix
+    res = confusion_matrix(test_label, results, labels=[0,1,2,3,4,5,6,7,8,9])#"metal", "pop", "disco", "blues", "classical", "reggae", "rock", "hiphop", "country", "jazz"]) 
+    class_names = [0,1,2,3,4,5,6,7,8,9]
+    plot_confusion_matrix(res, class_names)
+    plt.show()
+    '''
 
-data = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/data.npy')
-labels = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/labels.npy')
-print(data)
-
-#train_data, test_data, train_label, test_label = construct_train_test_data_grayscale(324,300,len(labels),data, labels, 0.1)
-
-train_data, test_data, train_label, test_label = train_test_split(data, labels, test_size=0.10)
-np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/train_data', train_data)
-np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/train_label', train_label)
-np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/test_data', test_data)
-np.save('/Users/iremergun/Desktop/ucr_classes/cs235/proj/test_label', test_label)
-print(train_data.shape)
-print(test_data.shape)
-print(test_label)
-print(train_label)
-'''
-train_data = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/train_data.npy') 
-test_data = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/test_data.npy') 
-train_label = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/train_label.npy') 
-test_label = np.load('/Users/iremergun/Desktop/ucr_classes/cs235/proj/test_label.npy') 
-print(train_data.shape)
-print(test_data.shape)
-
-
-model = LeNet.build_model_lenet(324,300,3,10)
-opt = Adam(lr=0.001)
-model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"]) #categorical_crossentropy
-#train_data = train_data.reshape((train_data.shape[0], 324, 300, 1))
-#test_data = test_data.reshape((test_data.shape[0], 324, 300, 1))
-earlystop = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=8, \
-                          verbose=1, mode='auto')
-callbacks_list = [earlystop]
-#plot_model(model, to_file='/Users/iremergun/Desktop/ucr_classes/cs235/proj/report/model.png', show_shapes=True)
-
-print("[INFO] training...")
-model.fit(train_data, train_label, batch_size=64, epochs=10, verbose=1, validation_data=(test_data, test_label) )
-(loss, accuracy) = model.evaluate(test_data, test_label, batch_size=64, verbose=1)
-results = model.predict_classes(test_data, batch_size=64, verbose =1)
-print("[INFO] accuracy: {:.2f}%".format(accuracy * 100))
-print("loss: {}".format(loss))
-res = confusion_matrix(test_label, results, labels=[0,1,2,3,4,5,6,7,8,9])#"metal", "pop", "disco", "blues", "classical", "reggae", "rock", "hiphop", "country", "jazz"]) 
-class_names = [0,1,2,3,4,5,6,7,8,9]
-plot_confusion_matrix(res, class_names)
-plt.show()
+if __name__ == "__main__":
+    flags = parser.parse_args(sys.argv[1:])
+    main(flags.dataset_folder_path)
